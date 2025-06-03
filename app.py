25f0b1da9259e3b8566acd182cfa693c98d286bd
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-06-03 13:00:25 +0200

    feat(transcription): integrate OpenAI API for audio transcription with Streamlit session state handling

--- app.py
+++ app.py
@@ -1,9 +1,59 @@
 from io import BytesIO
-
 import streamlit as st
 from audiorecorder import audiorecorder
 
-st.set_page_config(page_title="Notes AI", layout="centered")
+from openai import OpenAI
+from dotenv import dotenv_values
+
+#
+# INIT
+#
+
+if "note_audio_bytes" not in st.session_state:
+    st.session_state["note_audio_bytes"] = None
+
+if "note_audio_text" not in st.session_state:
+    st.session_state["note_audio_text"] = ""
+
+if "note_text" not in st.session_state:
+    st.session_state["note_text"] = ""
+
+#
+# AUDIO TRANSCRIBE
+#
+
+AUDIO_TRANSCRIBE_MODEL = "whisper-1"
+
+env = dotenv_values(".env")
+
+
+@st.cache_resource
+def get_openai_client():
+    return OpenAI(api_key=env["OPENAI_API_KEY"])
+
+
+def transcribe_audio(audio_bytes):
+    openai_client = get_openai_client()
+    audio_file = BytesIO(audio_bytes)
+    audio_file.name = "audio.mp3"
+    transcript = openai_client.audio.transcriptions.create(
+        file=audio_file,
+        model=AUDIO_TRANSCRIBE_MODEL,
+        response_format="verbose_json",
+    )
+
+    return transcript.text
+
+
+#
+# MAIN
+#
+
+st.set_page_config(
+    page_title="Notes AI",
+    page_icon=":memo:",
+    layout="centered",
+)
 st.title(":memo: Notes AI")
 st.markdown("*Capture your thoughts with voice or text*")
 st.divider()
@@ -16,16 +66,37 @@ input_mode = st.radio(
 
 if input_mode == ":microphone: Audio Recording":
     note_audio = audiorecorder(
-        start_prompt="Start recording", stop_prompt="Stop Recording"
+        start_prompt="Start recording",
+        stop_prompt="Stop Recording",
     )
 
     if note_audio:
         audio = BytesIO()
-        note_audio.export(audio, format="mp3")
-        note_audio_bytes = audio.getvalue()
-        st.audio(note_audio_bytes, format="audio/mp3")
+        note_audio.export(
+            audio,
+            format="mp3",
+        )
+        st.session_state["note_audio_bytes"] = audio.getvalue()
+        st.audio(
+            st.session_state["note_audio_bytes"],
+            format="audio/mp3",
+        )
+
+        if st.button("Transcribe audio"):
+            st.session_state["note_audio_text"] = transcribe_audio(
+                st.session_state["note_audio_bytes"],
+            )
+
+        if st.session_state["note_audio_bytes"]:
+            st.text_area(
+                "Transcribed audio",
+                value=st.session_state["note_audio_text"],
+                # disabled=True,
+            )
 
 else:
-    manual_note = st.text_area(
-        "Type your note:", height=200, placeholder="Start typing your note here..."
+    st.session_state["note_text"] = st.text_area(
+        "Type your note:",
+        height=200,
+        placeholder="Start typing your note here...",
     )
